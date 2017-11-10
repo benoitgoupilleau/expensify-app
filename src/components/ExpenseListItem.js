f6ba11d8115ba5c112b3f14b4040a42bee5baf73
benoitgoupilleau <benoit.goupilleau@gmail.com>
2017-11-10 18:47:08 +0100

    Updated ExpenseList component

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
-    <Link to={`/edit/${id}`}><h3>Description: {description}</h3></Link>
-    <p>Amount: {amount}</p>
-    <p>CreatedAt: {createdAt}</p>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {numeral(amount / 100).format('0 0.00$')}
+       - 
+      {moment(createdAt).format('Do MMMM YYYY')}</p>
   </div>
 );
 
 export default ExpenseListItem;
+
+numeral.register('locale', 'fr', {
+  delimiters: {
+    thousands: ' ',
+    decimal: ','
+  },
+  abbreviations: {
+    thousand: 'k',
+    million: 'm',
+    billion: 'b',
+    trillion: 't'
+  },
+  ordinal : function (number) {
+    return number === 1 ? 'er' : 'ème';
+  },
+  currency: {
+    symbol: '€'
+  }
+});
+
+numeral.locale('fr');
