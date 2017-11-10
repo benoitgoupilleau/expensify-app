b6b094d13a2aecde7eeaf7a5cb167be3a796350e
benoitgoupilleau <benoit.goupilleau@gmail.com>
2017-11-10 15:42:34 +0100

    Removed dummy data

--- src/app.js
+++ src/app.js
@@ -4,20 +4,12 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 200, createdAt: 100 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 12000, createdAt: 0 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 10200, createdAt: 1000 }));
 
 const jsx = (
   <Provider store={store} >
